67e2ef77a43f234fe175968b3f739825f91e59c2
nguyenminhtri666 <ruaconminhtri2015@gmail.com>
2023-08-31 16:28:07 +0700

    1111

--- index.js
+++ index.js
@@ -1,96 +1,30 @@
-const image = document.getElementById('cover'),
-    title = document.getElementById('music-title'),
-    artist = document.getElementById('music-artist'),
-    currentTimeEl = document.getElementById('current-time'),
-    durationEl = document.getElementById('duration'),
-    progress = document.getElementById('progress'),
-    playerProgress = document.getElementById('player-progress'),
-    prevBtn = document.getElementById('prev'),
-    nextBtn = document.getElementById('next'),
-    playBtn = document.getElementById('play'),
-    background = document.getElementById('bg-img');
-
-const music = new Audio();
-
-const songs = [
-    {
-        path: '1.mp3.mp3',
-        displayName: 'Happy Birthday to You',
-        cover: '1.jpg',
-        artist: 'AMEE x Hoàng Dũng',
-    },
-    {
-        path: '3.mp3.mp3',
-        displayName: 'Sinh Nhật',
-        cover: '3.jpg',
-        artist: 'Hieuthuhai, Manbo, HurryKng',
-    },
-];
-
-let musicIndex = 0;
-let isPlaying = false;
-
-function togglePlay() {
-    if (isPlaying) {
-        pauseMusic();
-    } else {
-        playMusic();
+const options = {
+    accessibility: true,
+    prevNextButtons: true,
+    pageDots: true,
+    setGallerySize: false,
+    arrowShape: {
+        x0: 1,
+        x1: 58,
+        y1: 62,
+        x2: 55,
+        y2: 48,
+        x3: 18
     }
+};
+
+// Function to set background position forr slides
+function setBgPosition(slide, index) {
+    const x = -(slide.target + flkty.x) / 3;
+    slides[index].style.backgroundPosition = `${x}px`;
 }
 
-function playMusic() {
-    isPlaying = true;
-    // Change play button icon
-    playBtn.classList.replace('fa-play', 'fa-pause');
-    // Set button hover title
-    playBtn.setAttribute('title', 'Pause');
-    music.play();
-}
+// Slides initialization
+const carousel = document.querySelector('[carousel]');
+const slides = Array.from(document.getElementsByClassName('carousel-cell'));
+const flkty = new Flickity(carousel, options);
 
-function pauseMusic() {
-    isPlaying = false;
-    // Change pause button icon
-    playBtn.classList.replace('fa-pause', 'fa-play');
-    // Set button hover title
-    playBtn.setAttribute('title', 'Play');
-    music.pause();
-}
-
-function loadMusic(song) {
-    music.src = song.path;
-    title.textContent = song.displayName;
-    artist.textContent = song.artist;
-    image.src = song.cover;
-    background.src = song.cover;
-}
-
-function changeMusic(direction) {
-    musicIndex = (musicIndex + direction + songs.length) % songs.length;
-    loadMusic(songs[musicIndex]);
-    playMusic();
-}
-
-function updateProgressBar() {
-    const { duration, currentTime } = music;
-    const progressPercent = (currentTime / duration) * 100;
-    progress.style.width = `${progressPercent}%`;
-
-    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
-    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
-    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
-}
-
-function setProgressBar(e) {
-    const width = playerProgress.clientWidth;
-    const clickX = e.offsetX;
-    music.currentTime = (clickX / width) * music.duration;
-}
-
-playBtn.addEventListener('click', togglePlay);
-prevBtn.addEventListener('click', () => changeMusic(-1));
-nextBtn.addEventListener('click', () => changeMusic(1));
-music.addEventListener('ended', () => changeMusic(1));
-music.addEventListener('timeupdate', updateProgressBar);
-playerProgress.addEventListener('click', setProgressBar);
-
-loadMusic(songs[musicIndex]);
+// Event listener using bg position
+flkty.on('scroll', () => {
+    flkty.slides.forEach(setBgPosition);
+});
